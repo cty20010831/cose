a2ea6f5f9713b6fc6fdf011dff2fc774d15d6e72
cty20010831 <congtianyue233@gmail.com>
2024-02-06 18:44:43 -0600

    Created a mistake in defining environment variables

--- environment_configuration.sh
+++ environment_configuration.sh
@@ -4,15 +4,15 @@
 # It includes activating the virtual environment, updating PYTHONPATH and setting environment variables.
 
 # Activate the virtual environment
-source venv_cose/bin/activate
+source ../.venv/bin/activate
 echo "Activated virtual environment venv_cose."
 
 export PROJECT_CODE_PATH="/Users/samcong/Library/CloudStorage/OneDrive-TheUniversityofChicago/Thesis/cose"
 export PYTHONPATH="${PYTHONPATH}:${PROJECT_CODE_PATH}"
 
-export COSE_DATA_DIR="${PYTHONPATH}/data_dir/quick_draw"
-export COSE_LOG_DIR="${PYTHONPATH}/log_dir/quick_draw"
-export COSE_EVAL_DIR="${PYTHONPATH}/eval_dir/quick_draw"
+export COSE_DATA_DIR="${PROJECT_CODE_PATH}/data_dir/quick_draw"
+export COSE_LOG_DIR="${PROJECT_CODE_PATH}/log_dir/quick_draw"
+export COSE_EVAL_DIR="${PROJECT_CODE_PATH}/eval_dir/quick_draw"
 
 echo "Environment variables set:"
 echo "PYTHONPATH=$PYTHONPATH"
@@ -22,4 +22,5 @@ echo "COSE_EVAL_DIR=$COSE_EVAL_DIR"
 
 # Run the code in the terminal 
 # chmod +x environment_configuration.sh
-# ./environment_configuration.sh 
+# source environment_configuration.sh 
+# conda deactivate
