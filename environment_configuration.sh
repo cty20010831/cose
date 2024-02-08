575cb78ad7be93ff5172148da4941c7dcd946e5e
cty20010831 <congtianyue233@gmail.com>
2024-02-08 10:29:52 -0600

    Update environment configuration change

--- environment_configuration.sh
+++ environment_configuration.sh
@@ -10,17 +10,19 @@ echo "Activated virtual environment venv_cose."
 export PROJECT_CODE_PATH="/Users/samcong/Library/CloudStorage/OneDrive-TheUniversityofChicago/Thesis/cose"
 export PYTHONPATH="${PYTHONPATH}:${PROJECT_CODE_PATH}"
 
-export COSE_DATA_DIR="${PROJECT_CODE_PATH}/data_dir/quick_draw"
-export COSE_LOG_DIR="${PROJECT_CODE_PATH}/log_dir/quick_draw"
-export COSE_EVAL_DIR="${PROJECT_CODE_PATH}/eval_dir/quick_draw"
+export COSE_DATA_DIR="${PROJECT_CODE_PATH}/data_dir"
+export COSE_LOG_DIR="${PROJECT_CODE_PATH}/log_dir"
+export COSE_EVAL_DIR="${PROJECT_CODE_PATH}/eval_dir"
+export GDRIVE_API_KEY=""
 
 echo "Environment variables set:"
 echo "PYTHONPATH=$PYTHONPATH"
 echo "COSE_DATA_DIR=$COSE_DATA_DIR"
 echo "COSE_LOG_DIR=$COSE_LOG_DIR"
 echo "COSE_EVAL_DIR=$COSE_EVAL_DIR"
+echo "GDRIVE_API_KEY=$GDRIVE_API_KEY"
 
 # Run the code in the terminal 
-# chmod +x environment_configuration.sh
-# source environment_configuration.sh 
+# chmod +x cose/environment_configuration.sh
+# source cose/environment_configuration.sh 
 # conda deactivate
